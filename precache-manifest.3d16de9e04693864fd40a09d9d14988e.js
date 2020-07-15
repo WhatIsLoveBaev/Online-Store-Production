7fce7fdddf6f1be1709eb44ceff13a29cd50bfc0
WhatIsLove <z@z.ru>
2020-07-15 21:36:48 +0400

    Updates

---
+++ precache-manifest.3d16de9e04693864fd40a09d9d14988e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "97cc16ff76ad18eff444",
+    "url": "/Online-Store-Production/static/css/main.9b714ebf.chunk.css"
+  },
+  {
+    "revision": "97cc16ff76ad18eff444",
+    "url": "/Online-Store-Production/static/js/main.97cc16ff.chunk.js"
+  },
+  {
+    "revision": "4a3e427334b5b69350b8",
+    "url": "/Online-Store-Production/static/js/1.4a3e4273.chunk.js"
+  },
+  {
+    "revision": "e6d6b7e1eceb17af9c78",
+    "url": "/Online-Store-Production/static/js/runtime~main.e6d6b7e1.js"
+  },
+  {
+    "revision": "6c052b7693bfae3abef5378ab795dafa",
+    "url": "/Online-Store-Production/static/media/phone.6c052b76.jpg"
+  },
+  {
+    "revision": "06684a8b9162bd7ceae69699062f209c",
+    "url": "/Online-Store-Production/static/media/monitor.06684a8b.jpg"
+  },
+  {
+    "revision": "9127633dca8e54a2a2ffdd8a08a13678",
+    "url": "/Online-Store-Production/static/media/tablet.9127633d.jpg"
+  },
+  {
+    "revision": "1a653f3126cc0168240b03ec4d509a9b",
+    "url": "/Online-Store-Production/static/media/camera.1a653f31.jpg"
+  },
+  {
+    "revision": "8fca6199efb6efbc8219ac4b263b8160",
+    "url": "/Online-Store-Production/static/media/keyboard.8fca6199.jpg"
+  },
+  {
+    "revision": "d9d50fbf29876ffcdd6a0b598679ade9",
+    "url": "/Online-Store-Production/static/media/iphone_video.d9d50fbf.mp4"
+  },
+  {
+    "revision": "4bbf68d0ca9acf350075b54f65483689",
+    "url": "/Online-Store-Production/static/media/samsung_video.4bbf68d0.mp4"
+  },
+  {
+    "revision": "8392114f8310326712e02381c3f60783",
+    "url": "/Online-Store-Production/static/media/iphone.8392114f.jpg"
+  },
+  {
+    "revision": "4acc56db4376fc526c9f76594c7aa463",
+    "url": "/Online-Store-Production/static/media/samsung.4acc56db.jpg"
+  },
+  {
+    "revision": "ff4a7fdb9a09b0b874895eb8ca707737",
+    "url": "/Online-Store-Production/static/media/xiaomi.ff4a7fdb.jpg"
+  },
+  {
+    "revision": "b0e07e7ce1429fd6fd3b785801193554",
+    "url": "/Online-Store-Production/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Online-Store-Production/precache-manifest.3d16de9e04693864fd40a09d9d14988e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Online-Store-Production/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
